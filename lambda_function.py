33efa681796215d91f3186213352fbd62cdc76a1
Vishalldwivedi <vdwivedi0109@gmail.com>
2026-02-25 16:58:44 +0530

    fixed

--- lambda_function.py
+++ lambda_function.py
@@ -1,1 +1,1 @@
-def lambda_handler(event, context): return {'statusCode': 200, 'body': 'You are very late for this task'}
+def lambda_handler(event, context): return {'statusCode': 200, 'body': 'You are very late for this task no sir i am still learning :)'}

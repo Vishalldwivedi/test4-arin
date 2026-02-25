1ab323208ef29636ce0806ef6749134e176c1320
Vishalldwivedi <vdwivedi0109@gmail.com>
2026-02-25 18:06:50 +0530

    all donedenadone

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
   return {
     'statusCode': 200, 
-    'body': 'You are very late for this task no sir i am still learning done sir its been a while hi there  :)'
+    'body': 'You are very late for this task no sir i am still learning done sir its been a while hi there hi hi there  :)'
   }

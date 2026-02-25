44d5bd075f206fa096a2f674fd91a1cc8a440827
Vishalldwivedi <vdwivedi0109@gmail.com>
2026-02-26 00:30:55 +0530

    commits added

--- main.tf
+++ main.tf
@@ -10,20 +10,20 @@ module "lambda" {
   lambda_role_name     = var.lambda_role_name
   lambda_handler       = var.lambda_handler
   lambda_runtime       = var.lambda_runtime
-  lambda_bucket        = module.s3.lambda_bucket_id
+  lambda_bucket        = module.s3.lambda_bucket_id # from output 
   lambda_s3_key        = var.lambda_s3_key
 }
 
 module "apigateway" {
   source               = "./modules/apigateway"
   api_name             = var.api_name
-  lambda_arn           = module.lambda.lambda_function_arn
+  lambda_arn           = module.lambda.lambda_function_arn# from output
   lambda_function_name = var.lambda_function_name
 }
 
 module "codepipeline" {
   source                  = "./modules/codepipeline"
-  artifact_bucket         = module.s3.artifact_bucket_id
+  artifact_bucket         = module.s3.artifact_bucket_id # from output
   codestar_connection_arn = var.codestar_connection_arn
   repo_name               = var.repo_name
   branch                  = var.branch
